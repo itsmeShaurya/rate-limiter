52538f433f498cfb39a0910b6a03678e8c039225
Shaurya pratap singh <shauryapratap251@gmail.com>
2026-07-18 02:17:46 +0530

    feat: implement sliding window rate limiter and strategy factory

--- src/main/java/com/shaurya/rate_limiter/factory/RateLimiterFactory.java
+++ src/main/java/com/shaurya/rate_limiter/factory/RateLimiterFactory.java
@@ -4,25 +4,26 @@ import com.shaurya.rate_limiter.config.RateLimiterProperties;
 import com.shaurya.rate_limiter.strategy.FixedWindowRateLimiter;
 import com.shaurya.rate_limiter.strategy.RateLimiter;
 import com.shaurya.rate_limiter.strategy.SlidingWindowRateLimiter;
+import org.springframework.stereotype.Component;
 
+import java.util.Map;
 
+@Component
 public class RateLimiterFactory {
-    private final FixedWindowRateLimiter fixedWindowRateLimiter;
-    private final SlidingWindowRateLimiter slidingWindowRateLimiter;
+    private final Map<String, RateLimiter> rateLimiters;
     private final RateLimiterProperties properties;
 
-    public RateLimiterFactory(FixedWindowRateLimiter fixedWindowRateLimiter, SlidingWindowRateLimiter slidingWindowRateLimiter, RateLimiterProperties properties) {
-        this.fixedWindowRateLimiter = fixedWindowRateLimiter;
-        this.slidingWindowRateLimiter = slidingWindowRateLimiter;
+    public RateLimiterFactory(Map<String, RateLimiter> rateLimiters, RateLimiterProperties properties) {
+        this.rateLimiters = rateLimiters;
         this.properties = properties;
     }
 
     public RateLimiter getRateLimiter(){
         switch (properties.getAlgorithm().toLowerCase()){
             case "fixed-window":
-                return fixedWindowRateLimiter;
+                return rateLimiters.get("fixedWindowRateLimiter");
             case "sliding-window":
-                return slidingWindowRateLimiter;
+                return rateLimiters.get("slidingWindowRateLimiter");
             default:
                 throw new IllegalArgumentException(
                         "Unknown algorithm: " + properties.getAlgorithm()
--- src/main/java/com/shaurya/rate_limiter/strategy/SlidingWindowRateLimiter.java
+++ src/main/java/com/shaurya/rate_limiter/strategy/SlidingWindowRateLimiter.java
@@ -2,10 +2,13 @@ package com.shaurya.rate_limiter.strategy;
 
 import com.shaurya.rate_limiter.config.RateLimiterProperties;
 import com.shaurya.rate_limiter.model.slidingwindow.SlidingWindowBucket;
+import org.springframework.stereotype.Service;
 
+import java.util.Deque;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Service
 public class SlidingWindowRateLimiter implements RateLimiter{
 
     private final RateLimiterProperties rateLimiterProperties;
@@ -19,6 +22,27 @@ public class SlidingWindowRateLimiter implements RateLimiter{
 
     @Override
     public boolean allowRequests(String userId) {
+        SlidingWindowBucket bucket = userBuckets.computeIfAbsent(userId, key -> new SlidingWindowBucket());
+        synchronized (bucket){
+            long currentTime = System.currentTimeMillis();
+            long windowSizeMillis = rateLimiterProperties.getWindowSizeSeconds() * 1000;
+            
+            // Calculate the start of the valid window
+            long windowStart = currentTime - windowSizeMillis;
+
+            Deque<Long> timestamps = bucket.getRequestTimeStamps();
+
+            // Remove all expired requests
+            while(!timestamps.isEmpty() && timestamps.peekFirst() < windowStart){
+                timestamps.removeFirst();
+            }
+
+            // Check if user can make another request
+            if(timestamps.size() < rateLimiterProperties.getMaxRequests()){
+                timestamps.addLast(currentTime);
+                return true;
+            }
+        }
         return false;
     }
 }
